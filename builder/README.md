e2983dd96c74b98a33a56a6e204ca304738d3f32
criro1 <crirothebest@list.ru>
2020-07-13 13:04:31 +0300

    builder

---
+++ builder/README.md
@@ -0,0 +1,1 @@
+cmd/builder/main.go is testing of package builder from pkg/builder/builder.go
---
+++ builder/cmd/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+
+	b "github.com/criro1/wildberries/builder/pkg"
+)
+
+func main() {
+	expect := "The fish in rolls is tuna.\nThe vegetables in rolls are cucumber, asparagus.\nThe sause in rolls is wasabi."
+	
+	prod := new(b.Product)
+	
+	director := b.Director{&(b.RollsBuilder{prod})}
+	director.Construct("tuna", "cucumber, asparagus", "wasabi")
+
+	res, err := prod.GetIngridients()
+	if err != nil {
+		fmt.Println("Error")
+	}
+	if expect != res {
+		fmt.Println("expect != res")
+	} else {
+		fmt.Println("Everything is OK, expect == result\n\n" + res)
+	}
+	
+}
