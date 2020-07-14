6048e3d1e2e220095809733e78473e596499ccaf
criro1 <crirothebest@list.ru>
2020-07-14 11:22:07 +0300

    facade/pkg/referee/referee_test.go

--- facade/pkg/referee/referee_test.go
+++ facade/pkg/referee/referee_test.go
@@ -10,31 +10,24 @@ func TestReferee(t *testing.T) {
 	r := NewReferee("Kassai", 0, 0)
 
 	expectedYellow := "Kassai shows yellow card to Fernandez"
-	resultYellow, err := r.ShowYellowCard("Fernandez")
-	if err != nil {
-		t.Error("Error:", err)
-	}
-	if expectedYellow != resultYellow {
-		t.Error("Error, expectedYellow != resultYellow")
-	}
-	r.ShowYellowCard("Valverde")
+	t.Run("ShowYellowCard", func(t *testing.T) {
+		resultYellow, err := r.ShowYellowCard("Fernandez")
+		assert.NoError(t, err, "unexpected error:", err)
+		assert.EqualValues(t, expectedYellow, resultYellow)
+	})
 
 	expectedRed := "Kassai shows red card to James Rodriguez"
-	resultRed, err := r.ShowRedCard("James Rodriguez")
-	if err != nil {
-		t.Error("Error:", err)
-	}
-	if expectedRed != resultRed {
-		t.Error("Error, expectedRed != resultRed")
-	}
-
+	t.Run("ShowRedCard", func(t *testing.T) {
+		resultRed, err := r.ShowRedCard("James Rodriguez")
+		assert.NoError(t, err, "unexpected error:", err)
+		assert.EqualValues(t, expectedRed, resultRed)
+	})
+	
 	expected := "Referee Kassai shows 2 yellow cards and 1 red cards in this match"
-	result, err := r.GetStatistic()
-	if err != nil {
-		t.Error("Error:", err)
-	}
-
-	if expected != result {
-		t.Error("Error, expected != result")
-	}
+	t.Run("ShowRedCard", func(t *testing.T) {
+		r.ShowYellowCard("Valverde")
+		result, err := r.GetStatistic()
+		assert.NoError(t, err, "unexpected error:", err)
+		assert.EqualValues(t, expected, result)
+	})
 }
