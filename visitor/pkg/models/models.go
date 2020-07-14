28ef85a7024906710701eeea44005d4a3fdb7045
criro1 <crirothebest@list.ru>
2020-07-14 19:04:07 +0300

    visitor

--- visitor/pkg/models/models.go
+++ visitor/pkg/models/models.go
@@ -3,6 +3,12 @@ package models
 // const ...
 const (
 	CustBuyPills = "Customer %s buy pills %s at pharmacy %s\n"
-	CustByuGoods = ""
-	Cust = ""
+	CustByuGoods = "Customer %s buy goods %s at maket %s\n"
+	CustGetHaircut = "Customer %s get haircut %s at barbershop %s\n"
+	BadPharName = "Bad pharmacy's name"
+	BadMarkName = "Bad market's name"
+	BadBarbName = "Bad barbershop's name"
+	Pill = "paracetamol"
+	Goods = "cheese, sausages, pasta"
+	Haircut = "Side Parted"
 )
--- visitor/pkg/visitor.go
+++ visitor/pkg/visitor.go
@@ -8,21 +8,24 @@ import (
 	mod "github.com/criro1/wildberries/visitor/pkg/models"
 )
 
-// Visitor interface ...
-type Visitor interface {
-	VisitPharmacy(p *pharmacy) string
-	VisitMarket(m *market) string
-	VisitBarbershop(b *barbershop) string
-}
-
-// Customer struct ...
-type Customer struct {
-	name string
+// CityInterface ...
+type CityInterface interface {
 }
 
 // Services interface ...
 type Services interface {
-	SellTo(c *Customer) string
+	SellTo(c *customer) string
+}
+
+// Visitor interface ...
+type Visitor interface {
+	VisitPharmacy(p *pharmacy, pill string) (str string, err error)
+	VisitMarket(m *market, goods string) (str string, err error)
+	VisitBarbershop(b *barbershop, haircut string) (str string, err error)
+}
+
+type customer struct {
+	name string
 }
 
 type pharmacy struct {
@@ -37,27 +40,67 @@ type barbershop struct {
 	name string
 }
 
-// VisitPharmacy ...
-func(c *Customer) VisitPharmacy(p *pharmacy, pill string) (string, error) {
-	return p.BuyPills(c.name, pill)
+type city struct {
+	name string
+	serv []Services
 }
 
-func(p *pharmacy) BuyPills(cust, pill string) (string, error) {
+func(p *pharmacy) SellTo(c customer) (str string, err error) {
+	return c.VisitPharmacy(p, mod.Pill)
+}
+
+func(c *customer) VisitPharmacy(p *pharmacy, pill string) (str string, err error) {
+	return p.buyPills(c.name, pill)
+}
+
+func(m *market) SellTo(c customer) (str string, err error) {
+	return c.VisitMarket(m, mod.Goods)
+}
+
+func(c *customer) VisitMarket(m *market, goods string) (str string, err error) {
+	return m.buyGoods(c.name, goods)
+}
+
+func(b *barbershop) SellTo(c customer) (str string, err error) {
+	return c.VisitBarbershop(b, mod.Haircut)
+}
+
+func(c *customer) VisitBarbershop(b *barbershop, haircut string) (str string, err error) {
+	return b.buyHaircut(c.name, haircut)
+}
+
+func(p *pharmacy) buyPills(cust, pill string) (str string, err error) {
 	if p.name == "" {
-		return "", errors.New("Bad pharmacy's name")
+		return str, errors.New(mod.BadPharName)
 	}
-	return fmt.Sprintf(mod.BuyPills, cust, pill, p.name), nil
+	return fmt.Sprintf(mod.CustBuyPills, cust, pill, p.name), nil
 }
 
-func(c *Customer) VisitMarket(m *market, goods string) (string, error) {
-	return m.BuyGoods(c.name, goods)
-}
-
-func(m *market) BuyGoods(cust, goods string) (string, error) {
-	if p.name == "" {
-		return "", errors.New("Bad market's name")
+func(m *market) buyGoods(cust, goods string) (str string, err error) {
+	if m.name == "" {
+		return str, errors.New(mod.BadMarkName)
 	}
-	return fmt.Sprintf(mod.name, cust, pill, p.name), nil
+	return fmt.Sprintf(mod.CustByuGoods, cust, goods, m.name), nil
 }
 
-func(c *Customer) VisitBarbershop(b *barbershop) string, error) {}
+func(b *barbershop) buyHaircut(cust, haircut string) (str string, err error) {
+	if b.name == "" {
+		return str, errors.New(mod.BadBarbName)
+	}
+	return fmt.Sprintf(mod.CustGetHaircut, cust, haircut, b.name), nil
+}
+
+// NewCity ...
+// func NewCity(name string) CityInterface {
+// 	return &city{
+// 		name: name,
+// 		serv: []Services:[pharmacy, market, barbershop]
+// 	}
+// }
+
+// NewCustomer ...
+// func NewCustomer(name string) Visitor {
+// 	return &customer{
+// 		name: name,
+// 	}
+// }
