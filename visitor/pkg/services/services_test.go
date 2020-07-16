30a7258700061d511ecbc1f144d1097298dca1a9
criro1 <crirothebest@list.ru>
2020-07-16 16:56:27 +0300

    visitor

--- visitor/pkg/services/services_test.go
+++ visitor/pkg/services/services_test.go
@@ -5,7 +5,7 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
-	// vis "github.com/criro1/wildberries/visitor/pkg"
+	vis "github.com/criro1/wildberries/visitor/pkg"
 )
 
 const (
@@ -25,6 +25,22 @@ const (
 	retBarber       = "Customer Nickolay got haircut at the barbershop `Viktorya`\n"
 	unexpectedError = "unexpected error:"
 )
+
+func TestDoPurchase(t *testing.T) {
+	t.Run(doPurchase, func(t *testing.T) {
+		visMock := new(vis.MockVis)
+
+		visMock.On(visitPharmacy, &Pharmacy{Name: yDoma}).Return(retPharmacy, nil).Once()
+		visMock.On(visitMarket, &Market{Name: spar}).Return(retMarker, nil).Once()
+		visMock.On(visitBarbershop, &Barbershop{Name: viktorya}).Return(retBarber, nil).Once()
+		
+		c := NewCity(madrid, yDoma, spar, viktorya)
+		result, err := c.DoPurchase(visMock)
+
+		assert.NoError(t, err, unexpectedError, err)
+		assert.EqualValues(t, expected, result)
+	})
+}
 
 func TestBuyPills(t *testing.T) {
 	t.Run(buyHaircut, func(t *testing.T) {
@@ -55,19 +71,3 @@ func TestBuyHaircut(t *testing.T) {
 		assert.EqualValues(t, "Customer Andrew got haircut at the barbershop `Viktorya`\n", res)
 	})
 }
-
-// func TestDoPurchase(t *testing.T) {
-// 	t.Run(doPurchase, func(t *testing.T) {
-// 		visMock := new(vis.MockVis)
-
-// 		visMock.On(visitPharmacy, &Pharmacy{Name: yDoma}).Return(retPharmacy, nil).Once()
-// 		visMock.On(visitMarket, &Market{Name: spar}).Return(retMarker, nil).Once()
-// 		visMock.On(visitBarbershop, &Barbershop{Name: viktorya}).Return(retBarber, nil).Once()
-		
-// 		c := NewCity(madrid, yDoma, spar, viktorya)
-// 		result, err := c.DoPurchase(visMock)
-
-// 		assert.NoError(t, err, unexpectedError, err)
-// 		assert.EqualValues(t, expected, result)
-// 	})
-// }
