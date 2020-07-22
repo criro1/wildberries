0a572b1150d70c01b20b6293d09ac4d87dd4f6af
criro1 <crirothebest@list.ru>
2020-07-22 17:21:42 +0300

    final

--- visitor/pkg/visitor/visitor_test.go
+++ visitor/pkg/visitor/visitor_test.go
@@ -5,7 +5,7 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
-	
+
 	"github.com/criro1/wildberries/visitor/pkg/barbershop"
 	"github.com/criro1/wildberries/visitor/pkg/market"
 	"github.com/criro1/wildberries/visitor/pkg/pharmacy"
